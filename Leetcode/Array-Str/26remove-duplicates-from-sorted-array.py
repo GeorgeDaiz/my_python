40c71dd7e44d30d85664b2ff19861004fa2d1d60
GeorgeDaiz <1353224609@qq.com>
2020-09-16 13:00:00 +0800

    2020/9/16

--- Leetcode/Array-Str/26remove-duplicates-from-sorted-array.py
+++ Leetcode/Array-Str/26remove-duplicates-from-sorted-array.py
@@ -1,19 +1,22 @@
 """
-Description：
+给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
 
-Given a sorted array, remove the duplicates in-place such that each element appear only once and return the new length.
-Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
 
-Translate：
+示例1:
 
-给定一个有序数组，删除重复内容，使每个元素只出现一次，并返回新的长度。
-不要为其他数组分配额外的空间，你必须在O（1）的空间里完成这个功能。
+给定数组 nums = [1,1,2], 
 
-Example:
-Given nums = [1,1,2],
-Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
-It doesn't matter what you leave beyond the new length.
+函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。 
 
+你不需要考虑数组中超出新长度后面的元素。
+示例2:
+
+给定 nums = [0,0,1,1,1,2,2,3,3,4],
+
+函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
+
+你不需要考虑数组中超出新长度后面的元素。
 """
 
 
