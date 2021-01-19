d42168c507e6f5f4ec9e192e1df5f672e6105f98
GeorgeDaiz <1353224609@qq.com>
2021-01-19 13:27:17 +0800

    2021/01/19

---
+++ Leetcode/Dynamic-Programming/264.ugly-number-ii.py
@@ -0,0 +1,30 @@
+"""
+264.丑数II
+编写一个程序，找出第 n 个丑数。
+
+丑数就是质因数只包含2, 3, 5 的正整数。
+
+示例:
+输入: n = 10
+输出: 12
+解释: 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 是前 10 个丑数。
+
+说明:
+1是丑数。
+n不超过1690。
+"""
+
+
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        res = [1, ]
+        t2, t3, t5 = 0, 0, 0
+        for i in range(n-1):
+            res.append(min(res[t2]*2, res[t3]*3, res[t5]*5))
+            if res[-1] == res[t2]*2:
+                t2 += 1
+            if res[-1] == res[t3]*3:
+                t3 += 1
+            if res[-1] == res[t5]*5:
+                t5 += 1
+        return res[-1]
--- Leetcode/剑指Offer/47.礼物的最大价值.py
+++ Leetcode/剑指Offer/47.礼物的最大价值.py
@@ -28,3 +28,14 @@ class Solution:
                 else:
                     grid[i][j] += max(grid[i-1][j], grid[i][j-1])
         return grid[-1][-1]
+
+    def maxValue1(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        for j in range(1, n):  # 初始化第一行
+            grid[0][j] += grid[0][j - 1]
+        for i in range(1, m):  # 初始化第一列
+            grid[i][0] += grid[i - 1][0]
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] += max(grid[i][j - 1], grid[i - 1][j])
+        return grid[-1][-1]
