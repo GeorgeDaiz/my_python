a4b5abbcaf24df42d949f0f09a628ff355bbc381
GeorgeDaiz <1353224609@qq.com>
2021-01-21 15:52:21 +0800

    2021/1/21

---
+++ Leetcode/Tree/226.invert-binary-tree.py
@@ -0,0 +1,51 @@
+"""
+226.翻转二叉树
+翻转一棵二叉树。
+
+示例：
+输入：
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+输出：
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        # 递归
+        if not root:
+            return
+        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        return root
+
+    def mirrorTree(self, root: TreeNode) -> TreeNode:
+        # 迭代
+        if not root:
+            return
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+            node.left, node.right = node.right, node.left
+        return root
