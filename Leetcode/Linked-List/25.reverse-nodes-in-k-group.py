091a36b4d447d2245d9aa85c315263393ae37d7d
GeorgeDaiz <1353224609@qq.com>
2021-02-18 15:28:28 +0800

    2021/2/18

---
+++ Leetcode/Linked-List/25.reverse-nodes-in-k-group.py
@@ -0,0 +1,55 @@
+"""
+25. K 个一组翻转链表
+给你一个链表，每k个节点一组进行翻转，请你返回翻转后的链表。
+
+k是一个正整数，它的值小于或等于链表的长度。
+
+如果节点总数不是k的整数倍，那么请将最后剩余的节点保持原有顺序。
+
+示例：
+给你这个链表：1->2->3->4->5
+
+当k= 2 时，应当返回: 2->1->4->3->5
+
+当k= 3 时，应当返回: 3->2->1->4->5
+
+说明：
+你的算法只能使用常数的额外空间。
+你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverse(self, head: ListNode, tail: ListNode):
+        prev = tail.next
+        p = head
+        while prev != tail:
+            nex = p.next
+            p.next = prev
+            prev = p
+            p = nex
+        return tail, head
+
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        hair = ListNode(0)
+        hair.next = head
+        pre = hair
+
+        while head:
+            tail = pre
+            for i in range(k):
+                tail = tail.next
+                if not tail:
+                    return hair.next
+            head, tail = self.reverse(head, tail)
+            pre.next = head
+            pre = tail
+            head = tail.next
+        return hair.next
