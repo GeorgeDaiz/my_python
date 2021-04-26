b03fe2cc3e51b56af7c743882eb6a80278baf7c5
GeorgeDaiz <1353224609@qq.com>
2021-04-26 09:47:35 +0800

    2021/04/26

--- Leetcode/Array-Str/41.first-missing-positive.py
+++ Leetcode/Array-Str/41.first-missing-positive.py
@@ -28,9 +28,7 @@ class Solution:
         n = len(nums)
         for i in range(n):
             while 1 <= nums[i] <= n and nums[nums[i]-1] != nums[i]:
-                temp = nums[nums[i] - 1]
-                nums[nums[i] - 1] = nums[i]
-                nums[i] = temp
+                nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
         for i in range(1, n + 1):
             if nums[i - 1] != i:
                 return i
