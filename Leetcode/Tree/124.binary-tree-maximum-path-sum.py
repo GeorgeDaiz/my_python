1a3f8efa1052df14ddc28f13b06bc2b765b6f66a
GeorgeDaiz <1353224609@qq.com>
2021-04-29 14:25:40 +0800

    2021/04/29

---
+++ Leetcode/Tree/124.binary-tree-maximum-path-sum.py
@@ -0,0 +1,50 @@
+"""
+124.二叉树中的最大路径和
+路径 被定义为一条从树中任意节点出发，沿父节点-子节点连接，达到任意节点的序列。同一个节点在一条路径序列中 至多出现一次 。该路径 至少包含一个 节点，且不一定经过根节点。
+
+路径和 是路径中各节点值的总和。
+
+给你一个二叉树的根节点 root ，返回其 最大路径和 。
+
+示例 1：
+输入：root = [1,2,3]
+输出：6
+解释：最优路径是 2 -> 1 -> 3 ，路径和为 2 + 1 + 3 = 6
+
+示例 2：
+输入：root = [-10,9,20,null,null,15,7]
+输出：42
+解释：最优路径是 15 -> 20 -> 7 ，路径和为 15 + 20 + 7 = 42
+
+提示：
+树中节点数目范围是 [1, 3 * 104]
+-1000 <= Node.val <= 1000
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def __init__(self):
+        self.max_sum = float("-inf")
+
+    def sum_re(self, node):
+        if not node:
+            return 0
+        left = max(self.sum_re(node.left), 0)
+        right = max(self.sum_re(node.right), 0)
+        newpath = node.val + left + right
+
+        self.max_sum = max(self.max_sum, newpath)
+
+        return node.val + max(left, right)
+
+    def maxPathSum(self, root: TreeNode):
+        self.sum_re(root)
+        return self.max_sum
---
+++ Leetcode/Tree/127.word-ladder.py
@@ -0,0 +1,81 @@
+"""
+127.单词接龙
+字典wordList 中从单词 beginWord和 endWord 的 转换序列 是一个按下述规格形成的序列：
+
+序列中第一个单词是 beginWord 。
+序列中最后一个单词是 endWord 。
+每次转换只能改变一个字母。
+转换过程中的中间单词必须是字典wordList 中的单词。
+给你两个单词 beginWord和 endWord 和一个字典 wordList ，找到从beginWord 到endWord 的 最短转换序列 中的 单词数目 。如果不存在这样的转换序列，返回 0。
+
+示例 1：
+输入：beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+输出：5
+解释：一个最短转换序列是 "hit" -> "hot" -> "dot" -> "dog" -> "cog", 返回它的长度 5。
+
+示例 2：
+输入：beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+输出：0
+解释：endWord "cog" 不在字典中，所以无法进行转换。
+
+提示：
+1 <= beginWord.length <= 10
+endWord.length == beginWord.length
+1 <= wordList.length <= 5000
+wordList[i].length == beginWord.length
+beginWord、endWord 和 wordList[i] 由小写英文字母组成
+beginWord != endWord
+wordList 中的所有字符串 互不相同
+"""
+from typing import List
+import collections
+
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]):
+        def addWord(word: str):
+            if word not in wordId:
+                nonlocal nodeNum
+                wordId[word] = nodeNum
+                nodeNum += 1
+
+        def addEdge(word: str):
+            addWord(word)
+            id1 = wordId[word]
+            chars = list(word)
+            for i in range(len(chars)):
+                tmp = chars[i]
+                chars[i] = "*"
+                newWord = "".join(chars)
+                addWord(newWord)
+                id2 = wordId[newWord]
+                edge[id1].append(id2)
+                edge[id2].append(id1)
+                chars[i] = tmp
+
+        wordId = dict()
+        edge = collections.defaultdict(list)
+        nodeNum = 0
+
+        for word in wordList:
+            addEdge(word)
+
+        addEdge(beginWord)
+        if endWord not in wordId:
+            return 0
+
+        dis = [float("inf")] * nodeNum
+        beginId, endId = wordId[beginWord], wordId[endWord]
+        dis[beginId] = 0
+
+        que = collections.deque([beginId])
+        while que:
+            x = que.popleft()
+            if x == endId:
+                return dis[endId] // 2 + 1
+            for it in edge[x]:
+                if dis[it] == float("inf"):
+                    dis[it] = dis[x] + 1
+                    que.append(it)
+
+        return 0
