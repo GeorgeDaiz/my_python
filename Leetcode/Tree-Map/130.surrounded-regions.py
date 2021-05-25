1477eaa6e48c9d75f9d1b61d0000725302f99a04
GeorgeDaiz <1353224609@qq.com>
2021-05-25 13:27:13 +0800

    2021/05/24

--- Leetcode/Tree-Map/130.surrounded-regions.py
+++ Leetcode/Tree-Map/130.surrounded-regions.py
@@ -27,7 +27,34 @@ from typing import List
 
 
 class Solution:
-    def solve(self, board: List[List[str]]) -> None:
+    def solve(self, board: List[List[str]]):
         """
         Do not return anything, modify board in-place instead.
         """
+        if not board:
+            return board
+        m, n = len(board), len(board[0])
+
+        def dfs(i, j):
+            if not 0 <= i < m or not 0 <= j < n or board[i][j] != 'O':
+                return
+            board[i][j] = 'A'
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
+
+        for i in range(m):
+            dfs(i, 0)
+            dfs(i, n-1)
+
+        for j in range(1, n-1):
+            dfs(0, j)
+            dfs(m-1, j)
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'A':
+                    board[i][j] = 'O'
+                elif board[i][j] == 'O':
+                    board[i][j] = 'X'
