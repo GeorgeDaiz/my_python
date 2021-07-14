880606046724b953f40e4ef48bd5813f8f57d27a
GeorgeDaiz <1353224609@qq.com>
2021-07-14 14:27:24 +0800

    2021/07/14

--- Leetcode/Dynamic-Programming/309.最佳买卖股票时机含冷冻期.py
+++ Leetcode/Dynamic-Programming/309.最佳买卖股票时机含冷冻期.py
@@ -31,6 +31,20 @@ class Solution:
 
         return max(f[n-1][1], f[n-1][2])
 
+    def maxProfit1(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
+
+        n = len(prices)
+        f0, f1, f2 = -prices[0], 0, 0
+        for i in range(1, n):
+            newf0 = max(f0, f2 - prices[i])
+            newf1 = f0 + prices[i]
+            newf2 = max(f1, f2)
+            f0, f1, f2 = newf0, newf1, newf2
+
+        return max(f1, f2)
+
 
 if __name__ == '__main__':
     print(Solution().maxProfit([1, 2, 3, 0, 2]))
